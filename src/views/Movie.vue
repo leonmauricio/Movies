503e097691f889c7404c6a797c623e867990de99
Mauricio Leon <mauriciomunoz011@gmail.com>
2018-12-20 17:23:50 -0300

    Added Normalize and fixed errors

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="search">
-    <h1>{{ movie.Title }}</h1>
-    <img :src="movie.Poster">
+  <div class="movie-detail">
+    <div v-if="movie">
+      <h1>{{ movie.Title }}</h1>
+      <img :src="movie.Poster">
+    </div>
+    <div v-else>
+      CARGANDO...
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   data() {
@@ -15,12 +20,17 @@ export default {
     };
   },
   methods: {
-    fetchMovie: function () {
-      axios.get(`http://www.omdbapi.com/?i=${this.$route.params.id}&apikey=db6050af`).then(response => {this.movie = response.data})
-    }
+    fetchMovie() {
+      axios
+        .get(`http://www.omdbapi.com/?i=${this.$route.params.id}&apikey=db6050af`)
+        .then((response) => {
+          this.movie = response.data;
+          return true;
+        });
+    },
   },
-  mounted: function () {
+  mounted() {
     this.fetchMovie();
-  }
+  },
 };
 </script>
