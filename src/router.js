3aca7bf0d27fcacde789dac2f0043675102e09af
Mauricio Leon <mauriciomunoz011@gmail.com>
2018-12-20 16:42:56 -0300

    Removed unused pages

--- src/router.js
+++ src/router.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
-import Item from './views/Item.vue';
 import Search from './views/Search.vue';
 import Movie from './views/Movie.vue';
 
@@ -14,21 +11,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-    {
-      path: '/items/:id',
-      name: 'item',
-      component: Item,
-    },
-    {
-      path: '/search',
       name: 'search',
       component: Search,
     },
--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page man man</h1>
-  </div>
-</template>
